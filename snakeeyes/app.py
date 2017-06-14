18f58b84c0aa294510bfe51b93c5f4d12e7a9035
Subramanian <vishwanath79@outlook.com>
2017-06-13 21:58:22 -0700

    blueprints

---
+++ snakeeyes/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from snakeeyes.blueprints.page import views
+
+
+def create_app(settings_override=None):
+    """
+    Create a Flask application using the app factory pattern.
+
+    :param settings_override: Override settings
+    :return: Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    #app.register_blueprint(page)
+    app.register_blueprint(views.page)
+
+    return app
